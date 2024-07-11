ca7d6fc1c913298d31db9acc2394c0f8edc38836
RHBoschman <145986827+RHBoschman@users.noreply.github.com>
2024-07-11 10:26:21 +0200

    updated better script

--- webapp/script2.js
+++ webapp/script2.js
@@ -15,36 +15,45 @@
 //   loop is not possible (would block all listeners), but
 //   you can do something like interval (chat gpt)
 
+const N_EXTRA_CMNDS = 30; // Should NOT be bigger then number of dumphis items!!!
+const REFRESH_VALUE = -1;
 
 const dumphisPath = '../exports/DumphisData.json';
 const unitInfoPath = '../exports/UnitInfoData.json';
+const funcDataPath = '../exports/FunctionData.json';
+
+// Global data objects to guarantee access for all code (not pretty, but functional for now)
+let dumphisDataObj = null;
+let unitInfoDataObj = null;
+let functionDataObj = null;
 
 $(document).ready(function() {
-    let selectedTimestamp = 0;
-    let selectedUnit = 0;
-
-    let dumphisDataObj = null;
-    let unitInfoDataObj = null;
+    let selectedTimestamp = 0, newSelTimestamp = 0;
+    let selectedUnit = 0, newSelUnit = 0;
+    let selectedCmnd = 0, newSelCmnd = 0;
 
     // Initialize app on button click
     $('#but').on('click', function() {
-        Promise.all([readJSON(dumphisPath), readJSON(unitInfoPath)])
-            .then(([dumphisData, unitInfoData]) => {
+        Promise.all([readJSON(dumphisPath), readJSON(unitInfoPath), readJSON(funcDataPath)])
+            .then(([dumphisData, unitInfoData, funcData]) => {
                 dumphisDataObj = dumphisData;
                 unitInfoDataObj = unitInfoData;
-                table_addTimestamps(dumphisData);
-                table_addUnits(unitInfoData);
+                functionDataObj = funcData;
+                table_addTimestamps();
+                table_addUnits();
             })
             .then(() => {
                 // Select all first items
-                if ($('#t_0').length > 0) {
+                if ($(`#t_${selectedTimestamp}`).length > 0) {
                     $('.timestamp-sel').removeClass('timestamp-sel');
-                    $('#t_0').addClass('timestamp-sel');
+                    $(`#t_${selectedTimestamp}`).addClass('timestamp-sel');
+                    selectedTimestamp = REFRESH_VALUE; // To make sure correct unit is selected immediately
                 }
 
-                if ($('#u_0').length > 0) {
+                if ($(`#u_${selectedUnit}`).length > 0) {
                     $('.unit-sel').removeClass('unit-sel');
-                    $('#u_0').addClass('unit-sel');
+                    $(`#u_${selectedUnit}`).addClass('unit-sel');
+                    selectedUnit = REFRESH_VALUE; // To make sure commands are displayed immediately
                 }
             })
             .catch(error => {
@@ -57,10 +66,8 @@ $(document).ready(function() {
     $('#main-table').on('click', '.timestamp', function() {
         for (let i = 0; i < dumphisDataObj.size; i++) {
             if ($(this).attr('id') === `t_${i}`) { // On GUI selected timestamp
-                // Select timestamp
-                $('.timestamp-sel').removeClass('timestamp-sel');
-                $(this).addClass('timestamp-sel');
-                selectedTimestamp = i;
+                newSelTimestamp = i;
+                break;
             }
         }
     });
@@ -68,22 +75,64 @@ $(document).ready(function() {
     $('#main-table').on('click', '.unit', function() {
         for (let i = 0; i < unitInfoDataObj.size; i++) {
             if ($(this).attr('id') === `u_${i}`) { // On GUI selected unit
-                // Select unit
-                $('.unit-sel').removeClass('unit-sel');
-                $(this).addClass('unit-sel');
-                selectedUnit = i;
+                newSelUnit = i;
+                break;
             }
         }
     });
 
-    // $('#main-table').on('click', '.cmnd', function() {
-
-    // });
+    $('#main-table').on('click', '.cmnd', function() {
+        for (let i = 0; i < n_createdCmnds; i++) {
+            if ($(this).attr('id') === `c_${i}`) { // On GUI selected command
+                newSelCmnd = i;
+                break;
+            }
+        }
+    });
 
     // $('#main-table').on('click', '.step', function() {
 
     // });
+
+    // Handle selections
+    setInterval(function() {
+        if (newSelTimestamp != selectedTimestamp) {
+            selectedTimestamp = newSelTimestamp;
+            console.log(`New timestamp selected: ${selectedTimestamp}`);
+            
+            $('.timestamp-sel').removeClass('timestamp-sel');
+            $(`#t_${selectedTimestamp}`).addClass('timestamp-sel');
+
+            // Select different unit
+            newSelUnit = findNewSelUnit(selectedTimestamp);
+        }
+
+        if (newSelUnit != selectedUnit) {
+            selectedUnit = newSelUnit;
+            console.log(`New unit selected: ${selectedUnit}`);
+
+            $('.unit-sel').removeClass('unit-sel');
+            $(`#u_${selectedUnit}`).addClass('unit-sel');
+
+            // Show commands
+            table_addCmnds(selectedUnit);
+            newSelCmnd = 0;
+            selectedCmnd = REFRESH_VALUE; // To make sure command selection is refreshed
+        }
+
+        if (newSelCmnd != selectedCmnd) {
+            selectedCmnd = newSelCmnd;
+            console.log(`New command selected: ${selectedCmnd}`);
+
+            $('.cmnd-sel').removeClass('cmnd-sel');
+            $(`#c_${selectedCmnd}`).addClass('cmnd-sel');
+
+            // Show steps
+        }
+    }, 1);
 });
+
+
 
 function readJSON(path) {
     return fetch(path)
@@ -101,8 +150,8 @@ function readJSON(path) {
         });
 }
 
-function table_addTimestamps(timestamps) {
-    timestamps.data.forEach((item, i) => {
+function table_addTimestamps() {
+    dumphisDataObj.data.forEach((item, i) => {
         let newRow = $(`<tr id="row_${i}">`);
         let cell = $(`<td id="t_${i}" class="timestamp">${item.time}</td>`);
 
@@ -111,9 +160,71 @@ function table_addTimestamps(timestamps) {
     });
 }
 
-function table_addUnits(units) {
-    units.data.forEach((item, i) => {
-        let cell = $(`<td id="u_${i}" class="unit">${item.name}</td>`);
+function table_addUnits() {
+    unitInfoDataObj.data.forEach((item, i) => {
+        let name = (item.name === "") ? "???" : item.name;
+        let cell = $(`<td id="u_${i}" class="unit">${name}</td>`);
         $(`#row_${i}`).append(cell)
     });
+}
+
+function table_addCmnds(selUnit) {
+    // Clean current displaying commands/steps
+    $('.step').remove();
+    $('.cmnd').remove();
+    $('.placeholder').remove();
+
+    let unitFuncId = unitInfoDataObj.data[selUnit].funcId;
+    for (let i = 0; i < functionDataObj.size; i++) {
+        let funcFuncId = functionDataObj.data[i].funcId;
+        if (funcFuncId === unitFuncId) {
+            curFuncIndex = i; // To be used for showing steps
+            n_createdCmnds = showCmnds(functionDataObj.data[i].cmnds);
+            break;
+        } else {
+            curFuncIndex = -1;
+        }
+    }
+}
+
+function table_addSteps() {
+
+}
+
+function findNewSelUnit(selTimestamp) {
+    let unitId = dumphisDataObj.data[selTimestamp].unitId;
+    let result = -1;
+    console.log(`Unit id: ${unitId}`);
+
+    unitInfoDataObj.data.forEach((item, i) => {
+        if (item.id === unitId) {
+            result = i;
+        }
+    });
+
+    if (result < 0) {
+        console.log("No unit is found!");
+        return 0;
+    }
+    else {
+        return result;
+    }
+    
+}
+
+function showCmnds(cmnds) {
+    let n_cmnds = 0;
+    cmnds.forEach((item, i) => {
+        let name = (item.name === "") ? "???" : item.name;
+        let cell = $(`<td id="c_${i}" class="cmnd">${name}</td>`);
+        $(`#row_${i}`).append(cell)
+        n_cmnds++;
+    });
+
+    for (let i = 0; i < (N_EXTRA_CMNDS - n_cmnds); i++) {
+        let cell = $('<td class="placeholder"></td>'); // Empty element
+        $(`#row_${n_cmnds + i}`).append(cell)
+    }
+
+    return n_cmnds;
 }
